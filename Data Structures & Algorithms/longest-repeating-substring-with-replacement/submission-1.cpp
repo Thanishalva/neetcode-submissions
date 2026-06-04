895cc7b98ebbafea6adc5933a443e4ba1233e666
Thanish Alva <alvathanish5@gmail.com>
2026-06-04 16:23:20 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+       int l=0;
+       int r=0;
+       int maxlen=0;
+       int maxf=0;
+       unordered_map <int,int> hash;
+
+
+       while(r<s.length()){
+        hash[s[r]-'A']++;
+
+        for(int i=0;i<26;i++){
+            maxf=max(maxf,hash[i]);
+        }
+
+        while((((r-l)+1)-maxf)>k){
+            hash[s[l]-'A']--;
+            l++;
+
+        }
+
+        maxlen=max(maxlen,r-l+1);
+        r++;
+
+
+
+
+
+
+
+
+
+       }
+       return maxlen;
+       
+    }
+};
